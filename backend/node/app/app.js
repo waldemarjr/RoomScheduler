7a7202c902daec7974ece4fbe78f712e280f4e3e
root <waldemarjr@gmail.com>
2020-06-04 20:28:26 +0000

    .

--- backend/node/app/app.js
+++ backend/node/app/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Teste');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Backend listening on port 3000!');
 });
